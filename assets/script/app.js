38a961086cd784ea1ac8765ba65c13a85bc7f019
Debashish Das <hello@debashish.dev>
2022-02-12 16:13:22 +0600

    Delete Button Working

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,10 +1,14 @@
 // setting C to clear LCD 
-
-
-function clear(){
-    document.getElementById('outputScreen').value = "";
+function clear() {
+    document.getElementById('outputScreen').value = 0;
 }
 
+
+// Del button 
+function del() {
+    document.getElementById('outputScreen').value = document.getElementById('outputScreen').value.slice(0, -1);
+}
+// Making button works 
 function display(n) {
     document.getElementById('outputScreen').value += n;
 }
