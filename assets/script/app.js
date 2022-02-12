c06926f43327396e573beab26ad9d669af8496cd
Debashish Das <hello@debashish.dev>
2022-02-12 16:07:30 +0600

    trying to fix output screen

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,10 @@
+// setting C to clear LCD 
+
+
+function clear(){
+    document.getElementById('outputScreen').value = "";
+}
+
 function display(n) {
     document.getElementById('outputScreen').value += n;
 }
