ecd2d61b5a703b10db6c59f73138f52eb729bb6f
Debashish D. Dev <hello@debashish.dev>
2022-02-12 21:37:02 +0600

    Changed Input to P tag

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,24 +1,24 @@
 // setting C to clear LCD 
 
 function clr() {
-    document.getElementById('outputScreen').value = '';
+    document.getElementById('outputScreen').innerText = '';
 }
 
 // Del button 
 function del() {
-    document.getElementById('outputScreen').value = document.getElementById('outputScreen').value.slice(0, -1);
+    document.getElementById('outputScreen').innerText = document.getElementById('outputScreen').value.slice(0, -1);
 }
 // Making button works 
 function display(n) {
-    document.getElementById('outputScreen').value += n;
+    document.getElementById('outputScreen').innerText += n;
 }
 
 // making the calculaton 
 function calc(){
     try{
-        document.getElementById('outputScreen').value = eval(document.getElementById('outputScreen').value);
+        document.getElementById('outputScreen').innerText = eval(document.getElementById('outputScreen').innerText);
     }
     catch{
-        document.getElementById('outputScreen').value = 'আমি ক্যালকুলটর হইলেও মানুষ ভাই!';
+        document.getElementById('outputScreen').innerText = 'আমি ক্যালকুলটর হইলেও মানুষ ভাই!';
     }
 }
